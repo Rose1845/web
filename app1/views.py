2bf70e9cd2de7563ea0cfedf7eb66a2dfdb49da0
Rose Odhiambo <odhiamborose466@gmail.com>
2022-10-23 13:10:26 +0300

    added the login.logout ,signup views

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render,HttpResponse
+from multiprocessing import AuthenticationError
+from django.shortcuts import redirect, render,HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
-def index(request):
-    return HttpResponse('HELLO DJANGO')
+
+
+def register(request):
+    if request.user.is_authenicated:
+        return redirect('/')
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return render(request,'home.html')
+    if request.method == 'POST':
+        username=request.POST['user']
+        password=request.POST['pass']
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request.user)
+            return redirect('/')
+            
+        else:
+            form= AuthenticationForm(request)
+            return render(
+                    request,'login.html',
+                    {'form',form}
+            )
+    else:
+        form=AuthenticationForm()
+        return render(request,'login.html',
+        {'form':form}
+        )
+
+def logout(request):
+    logout(request)
+    return redirect('/')
